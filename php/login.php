b239d155741eb9dd9feb40dbc2d3766c774b0e3e
mwright8928 <34581037+mwright8928@users.noreply.github.com>
2018-05-17 13:16:30 -0400

    Update login.php

--- php/login.php
+++ php/login.php
@@ -6,7 +6,7 @@ $submitted = !empty($_post);
 <html>
   <head> <title>Form Handler Page</tilte></head>
   <body>
-    <p>Form submitted?<?php> echo (int) $submitted; ?> </p>
+    <p>Form submitted?<?php echo (int) $submitted; ?> </p>
     <p>Your login info is: </p>
     <ul>
       <li><b>username</b>: <?php echo $_POST['username']; ?></li>
