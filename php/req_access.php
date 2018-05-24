d20009fe1426d736ace208b92b30784d75dd7c25
Abradley8885 <abradley8885@conestogac.on.ca>
2018-05-24 12:47:50 -0400

    Updated more request access stuff

--- php/req_access.php
+++ php/req_access.php
@@ -11,11 +11,11 @@ $submitted = !empty($_POST);
     <ul>
       	<li><b>First Name</b>: <?php echo $_POST['firstname']; ?></li>
       	<li><b>Last Name</b>: <?php echo $_POST['lastname']; ?></li>
-        <li>Email address: <?php echo $_POST['email']; ?></li>
-        <li>Birthdate: <?php echo $_POST['birthday']; ?></li>
-				<li><b>Faculty or Student </b>: <?php echo $_POST['fac_or_student']; ?></li>
+        <li><b>Email address</b>: <?php echo $_POST['email']; ?></li>
+        <li><b>Birthdate</b>: <?php echo $_POST['birthday']; ?></li>
+				<li><b>Faculty or Student</b>: <?php echo $_POST['fac_or_student']; ?></li>
         <li>
-          Other info:
+          <b>Other info</b>:
           <ul>
             <?php
              foreach ($_POST['involvement'] as $involve){
