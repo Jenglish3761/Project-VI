c8e314b589c7764b2a5810764ce1b138f7dc8a78
Jenglish3761 <39132245+Jenglish3761@users.noreply.github.com>
2018-05-31 17:19:37 -0400

    Update req_access.php

--- php/req_access.php
+++ php/req_access.php
@@ -27,6 +27,72 @@ $submitted = !empty($_POST);
          </ul>
        </li>
     </ul>
+	  
+	<?php
+	  
+	  if(isset($_POST['submit'])){
+		  $data_missing = array();
+		  
+		  if(emtpy($_POST['first_name'])){
+			  $data_missing[] = 'First Name';
+		  }else {
+			  $f_name = trim($_POST['first_name']);
+		  }
+		  if(emtpy($_POST['last_name'])){
+			  $data_missing[] = 'Last Name';
+		  }else {
+			  $l_name = trim($_POST['last_name']);
+		  }
+		  if(emtpy($_POST['email'])){
+			  $data_missing[] = 'Email';
+		  }else {
+			  $email = trim($_POST['email']);
+		  }
+		  if(emtpy($_POST['birthday'])){
+			  $data_missing[] = 'Birthday';
+		  }else {
+			  $birthday = trim($_POST['birthday']);
+		  }
+		  if(emtpy($_POST['type'])){
+			  $data_missing[] = 'Type';
+		  }else {
+			  $type = trim($_POST['type']);
+		  }
+		if(empty ($data_missing)){
+			require_once('mysqli_connect.php');
+			
+			$query = "INSERTT INTO req_access (first_name, last_name, email, birthday, type, id) 
+				VALUES (?,?,?,?,?, NULL)";
+			
+			$stmt = mysqli_prepare($dbc,$query);
+			
+			i Integers
+			d Doubles
+			b Blobs
+			s Everything Else
+				
+			mysqli_stmt_bind_param($stmt, "sssss", $f_name, $l_name, $email, $birthday, $type);
+			
+			mysqli_stmt_execute(stmt);
+			
+			$affected_rows = mysqli_stmt_affected_rows($stmt);
+			if($affected_rows == 1){
+				echo 'Person entered';
+				mysqli_stmt_close($stmt);
+				mysqli_close($dbc);
+			}else{
+				echo mysqli_error();
+				mysqli_stmt_close($stmt);
+				mysqli_close($dbc);
+			}
+		}else{
+			echo 'you missed some data';
+	  
+	  }
+	  
+	  
+	  
+	  ?>
   <p>Copyright &copy 2018 JAM</p>
   </body>
 </html>
