7845f54a9b4f2086480a8a6a74503dd01c446d93
Jenglish3761 <39132245+Jenglish3761@users.noreply.github.com>
2018-06-01 10:22:35 -0400

    Update mysqli_connect.php

--- php/mysqli_connect.php
+++ php/mysqli_connect.php
@@ -5,7 +5,7 @@ DEFINE ('DB_PASSWORD', 'jam');
 DEFINE ('DB_HOST', 'localhost');
 DEFINE ('DB_NAME', 'elevator');
 
-$dbc = @mysql_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME)
+$dbc = @mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME)
 OR die('Could not connect to MySQL ' . mysql_connect_error());
 
 ?>
