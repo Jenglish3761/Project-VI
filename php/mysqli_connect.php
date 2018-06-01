2b786f31e4f9647ab583f534e2fb2e774421c084
Jenglish3761 <39132245+Jenglish3761@users.noreply.github.com>
2018-06-01 11:51:58 -0400

    Update mysqli_connect.php

--- php/mysqli_connect.php
+++ php/mysqli_connect.php
@@ -1,7 +1,7 @@
 <?php
 
-DEFINE ('DB_USER', 'alex');
-DEFINE ('DB_PASSWORD', 'YES');
+DEFINE ('DB_USER', 'root');
+DEFINE ('DB_PASSWORD', '');
 DEFINE ('DB_HOST', 'localhost');
 DEFINE ('DB_NAME', 'elevator');
 
