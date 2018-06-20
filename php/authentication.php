895c63aa0ca66bb0bd2177f586957b5cdafa7299
mwright8928 <mwright8928@conestogac.on.ca>
2018-06-19 20:31:47 -0400

    Return to index.php on successful authentication

--- php/authentication.php
+++ php/authentication.php
@@ -12,6 +12,7 @@ while($row = mysqli_fetch_array($response)){ //while data is pulled fill table r
 
       if(strcmp($row['user'], $_POST['username'] ) == 0 && strcmp($row['pass'], $_POST['password'] == 0)){
         echo 'good';
+        echo "<a href='../index.php'>Homepage</a>";
       }
       else{
         echo 'bad';
