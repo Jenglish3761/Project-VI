565efb827fe2cd70fbb0d1bf88215ff86783516b
mwright8928 <mwright8928@conestogac.on.ca>
2018-06-26 21:08:27 -0400

    check queue for new messages before processing

--- ElevatorCode/elevator_supervisor/src/main.cpp
+++ ElevatorCode/elevator_supervisor/src/main.cpp
@@ -17,7 +17,11 @@
 #include "queue.h"
 #include "node.h"
 
-#define MOTOR_ID 0x101
+#define MOTOR_ID	0x101
+#define FLOOR_1		0x5
+#define FLOOR_2		0x6
+#define FLOOR_3		0x7
+
 
 
 int main()	{
@@ -32,9 +36,12 @@ int main()	{
 	int ID = 0x100;	//	Set transmit ID to 100 hex.
 	int enable = 0;
 
+	//	Send elevator to floor one upon start.
+	pcanTx(ID, FLOOR_1);
 
 	//	Loop forever.
 	for (;;)	{
+		//	Get CAN bus message.
 		Rxmsg = pcanRx(1);
 
 		// Add new message to queue.
@@ -46,43 +53,38 @@ int main()	{
 
 		//	Check if queue is empty. If not empty process message.
 		if(isQueueEmpty() == 0)	{
-			
-		}
+			printf("Enabled = 1, Disabled = 0  :  %d", enable);
+			if ((int)Rxmsg.ID != MOTOR_ID)	{
+				if(enable == 1)	{
+					switch (Rxmsg.DATA[0])	{
+						//	Floor 1.
+						case 0x5 :
+							pcanTx(ID, Rxmsg.DATA[0]);
+							enable = 0;
+						break;
+
+						//	Floor 2.
+						case 0x6 :
+							pcanTx(ID, Rxmsg.DATA[0]);
+							enable = 0;
+						break;
+
+						//	Floor 3.
+						case 0x7 :
+							p	canTx(ID, Rxmsg.DATA[0]);
+							enable = 0;
+						break;
+					}
 
 
-		printf("Enabled = 1, Disabled = 0  :  %d", enable);
-		if ((int)Rxmsg.ID != MOTOR_ID)
-		{
-			if(enable == 1){
-			switch (Rxmsg.DATA[0])	{
-				//	Floor 1.
-				case 0x5 :
-					pcanTx(ID, Rxmsg.DATA[0]);
-					enable = 0;
-				break;
-
-				//	Floor 2.
-				case 0x6 :
-					pcanTx(ID, Rxmsg.DATA[0]);
-					enable = 0;
-				break;
-
-				//	Floor 3.
-				case 0x7 :
-					pcanTx(ID, Rxmsg.DATA[0]);
-					enable = 0;
-				break;
+					//	Open and close elevator
+					if(Rxmsg.DATA[0] == 0x8)	{
+						enable = 1;
+					} else if (Rxmsg.DATA[0] == 0x9)	{
+		 				enable = 0;
+					}
+				}
 			}
-		 }
-		 //	Open and close elevator
-		 if(Rxmsg.DATA[0] == 0x8){
-			enable = 1;
-
-		 }else if (Rxmsg.DATA[0] == 0x9){
-			enable = 0;
-		 }
-
 		}
 	}
-
 }
