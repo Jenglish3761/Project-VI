2b9abf4b84486023ad697ae49e05c00b218b0c9c
mwright8928 <mwright8928@conestogac.on.ca>
2018-07-04 22:12:49 -0400

    keyboard event listener example from class

---
+++ js/keyboardEventListener.js
@@ -0,0 +1,8 @@
+var el;
+
+function charCount(el)  {
+  var textEntered, charCount, display, lastKey;
+  textEntered = document.getElementById('message').value;
+  display = document.getElementById('charsLeft');
+  
+}
